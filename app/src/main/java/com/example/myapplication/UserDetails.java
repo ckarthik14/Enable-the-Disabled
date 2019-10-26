aaf3cd091972471aa21babf6b339390c1a2274df
lordbeerus0505 <abhiram.natarajan@gmail.com>
2019-10-26 14:50:36 +0530

    users details added

---
+++ app/src/main/java/com/example/myapplication/UserDetails.java
@@ -0,0 +1,9 @@
+package com.example.myapplication;
+
+public class UserDetails {
+    static String username = "";
+    static String password = "";
+    static String chatWith = "";
+
+    static String lastmessage = "";
+}
